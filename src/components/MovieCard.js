b7fecfcf62a77420a07e82a5c09e3d611a65c6c4
Euller Braz <euller.braz99@gmail.com>
2021-07-06 17:40:13 -0300

    Requisito 3: Adicionado Conteúdo a MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id, imagePath, title, storyline, subtitle } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ title }</h1>
+        <h3>{ subtitle }</h3>
+        <p>{ storyline }</p>
+        <img src={ imagePath } alt={ title } />
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: {
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+  }.isRequired,
+};
 
 export default MovieCard;
