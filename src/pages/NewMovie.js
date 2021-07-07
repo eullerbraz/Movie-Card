b1855891ec73c808d2b12b7299aa5a4ab87e71c4
Euller Braz <euller.braz99@gmail.com>
2021-07-07 14:15:33 -0300

    Requisito 6: Adicionado Função de Criar um Filme

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-
-import MovieForm from '../components/MovieForm';
-/* import * as movieAPI from '../services/movieAPI'; */
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      status: '',
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
-    console.log(newMovie);
+  async handleSubmit(newMovie) {
+    this.setState({ status: 'loading' });
+    await movieAPI.createMovie(newMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
   }
 
   render() {
+    const { status, shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return (<Redirect to="/" />);
+    }
+
+    if (status === 'loading') {
+      return (<Loading />);
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
